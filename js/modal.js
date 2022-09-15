0d0a0e4730cb14b4218e326403efd2093ce67ff4
Brian Francoeur <francoeurbj@gmail.com>
2022-09-15 15:04:35 -0400

    create htnl, css, and js for modal component

--- js/modal.js
+++ js/modal.js
@@ -5,3 +5,16 @@
 // trigger modal display on click
 
 // close modal
+const elemModalButton = document.getElementById("open-modal");
+const elemModal = document.getElementById("modal");
+const elemCloseBtn = document.getElementById("close-btn");
+
+elemModalButton.addEventListener("click", () => {
+  elemModal.classList.add("open");
+  elemModal.classList.remove("close");
+});
+
+elemCloseBtn.addEventListener("click", () => {
+  elemModal.classList.add("close");
+  elemModal.classList.remove("open");
+});
